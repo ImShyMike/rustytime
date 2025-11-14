0d69ba192cce9e9fd732b47a4777392ec31901e3
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-11-14 18:19:03 +0000

    cache docker locally
    avoid caching 1gb of files every upload

--- upload.sh
+++ upload.sh
@@ -4,8 +4,8 @@ export DOCKER_BUILDKIT=1
 
 docker buildx build \
     --platform linux/amd64,linux/arm64/v8 \
-    --cache-from type=registry,ref=shymike/rustytime:cache \
-    --cache-to type=registry,ref=shymike/rustytime:cache,mode=max \
+    --cache-from type=local,src=./.buildx-cache \
+    --cache-to type=local,dest=./.buildx-cache,mode=max \
     --tag shymike/rustytime:latest \
     --push \
     .
