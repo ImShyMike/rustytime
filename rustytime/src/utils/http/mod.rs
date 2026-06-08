3c056d1499c9bfe681bee2214d9fa9640bca82b9
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2026-06-08 21:30:18 +0100

    fix!: user agent parsing

--- rustytime/src/utils/http/mod.rs
+++ rustytime/src/utils/http/mod.rs
@@ -14,26 +14,29 @@ use woothee::parser::Parser;
 use crate::utils::env::use_cloudflare_headers;
 
 static USER_AGENT_PATTERN: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r"(?iU)^(?:(?:wakatime|chrome|firefox|edge)\/(?:v?[\d+.]+|unset)?\s)(?:\(?(\w+)[-_].*\)?.+\s)?(?:([^\/\s]+)\/[\w\d\.]+\s)?([^\/\s]+)-wakatime\/.+$").unwrap()
+    Regex::new(r"wakatime/[^ ]+ \(([^)]+)\)(?: [^ ]+ ([^/]+)(?:/([^/]+))?)?").unwrap()
 });
 
 /// Parse user agent into OS and editor
 /// Based on https://github.com/muety/wakapi/blob/master/utils/http.go#L89-L127
 pub fn parse_user_agent(ua: String) -> Result<(Option<String>, Option<String>), String> {
+    if ua.trim().is_empty() {
+        return Err("failed to parse user agent string".to_string());
+    }
+
     // try to parse wakatime client user agents first
-    if let Some(groups) = USER_AGENT_PATTERN.captures(&ua)
-        && groups.len() == 4
-    {
-        // extract OS
-        let os = groups.get(1).map(|m| m.as_str().to_ascii_lowercase());
+    if let Some(groups) = USER_AGENT_PATTERN.captures(&ua) {
+        let os = groups
+            .get(1)
+            .and_then(|m| m.as_str().split('-').next())
+            .unwrap_or_default()
+            .to_ascii_lowercase();
+        let editor = groups
+            .get(2)
+            .map(|m| m.as_str().to_ascii_lowercase())
+            .unwrap_or_default();
 
-        // parse editor
-        let mut editor = groups.get(2).map(|m| m.as_str().to_ascii_lowercase());
-        if editor.is_none() {
-            editor = groups.get(3).map(|m| m.as_str().to_ascii_lowercase());
-        }
-
-        return Ok((os, editor));
+        return Ok((Some(os), Some(editor)));
     }
 
     // try to parse the browser user agent as a fallback using woothee
